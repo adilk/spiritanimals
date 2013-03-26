9bee033b0d96f4e744cab463f89a503208e7e108
Adil Kadir <adilk@vt.edu>
2013-03-26 16:24:19 -0400

    Added level design and player movement changes
    Added rocks and text for the fox spirit as well

--- Assets/FoxSpawnScript.js
+++ Assets/FoxSpawnScript.js
@@ -6,6 +6,8 @@ var player : Transform;
 var prefab : Transform;
 var spawn1 : Transform;
 var spawn_distance : int = 1;
+var fox_text_prefab : Transform;
+var text_offset : Vector3;
 
 function Start() {
 if (player == null) {
@@ -26,7 +28,23 @@ function Spawn(){
  spawning = false;
  
  //create the object at point of the location variable
- var thingToMake : Transform = Instantiate(prefab, spawn1.position, Quaternion.identity);
+ var foxInstance : Transform = Instantiate(prefab, spawn1.position, Quaternion.identity);
  
+ // Disable player movement
+ player.GetComponent(CharacterMotor).canControl = false;
+ 
+ // Fox Dialouge
+ var text_instance : Transform = Instantiate(fox_text_prefab, spawn1.position - text_offset, Quaternion.identity);
+ yield WaitForSeconds(3);
+ Destroy(text_instance.gameObject);
+ // Fox Movement
+ 
+ // Delete the fox
+ 
+ Destroy(foxInstance.gameObject);
+ 
+ 
+ // Re-enable player movement
+ player.GetComponent(CharacterMotor).canControl = true;
 
 }
