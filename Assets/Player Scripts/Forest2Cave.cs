7fc87d60b73f43eda458c6295b31a80883862db9
Adil Kadir <adilk@vt.edu>
2013-04-28 17:58:24 -0400

    integrated transition from forest to cave
    -turned off rain in cave
    -added pebbles in forest
    -added more leaf particles in forest

---
+++ Assets/Player Scripts/Forest2Cave.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+/*
+ * Defines a main menu consisting of an image which fades
+ * to white and loads the first level on mouse click.
+ */
+public class Forest2Cave : MonoBehaviour {
+	public Texture whiteTexture;
+	private bool fade = false;
+	private float alphaFadeValue = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnGUI() {		
+		if ( fade )
+		{
+			alphaFadeValue += Mathf.Clamp01(Time.deltaTime / 8);
+    		GUI.color = new Color(1, 1, 1, alphaFadeValue);
+    		GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height ), whiteTexture );
+		}
+		
+		if ( alphaFadeValue >= 0.85 )
+		{
+			Application.LoadLevel("cavescene");
+		}
+	}
+	
+	void OnTriggerEnter( Collider c )
+	{
+		fade = true;
+	}
+}
